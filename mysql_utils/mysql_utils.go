1e221864deefd4aadab03a5e41d12f555ea7b868
Guillermo Mugnaini <mugnainiguillermo@gmail.com>
2022-09-07 14:31:56 -0300

    initial commit

---
+++ mysql_utils/mysql_utils.go
@@ -0,0 +1,29 @@
+package mysql_utils
+
+import (
+	"errors"
+	"github.com/go-sql-driver/mysql"
+	"github.com/mugnainiguillermo/bookstore_utils-go/rest_errors"
+	"strings"
+)
+
+const (
+	ErrorNoRows = "no rows in result set"
+)
+
+func ParseError(err error) *rest_errors.RestErr {
+	sqlErr, ok := err.(*mysql.MySQLError)
+	if !ok {
+		if strings.Contains(err.Error(), ErrorNoRows) {
+			return rest_errors.NewNotFoundError("no record matching given id")
+		}
+		return rest_errors.NewInternalServerError("error parsing database response", errors.New("parsing error"))
+	}
+
+	switch sqlErr.Number {
+	case 1062:
+		return rest_errors.NewBadRequestError("invalid data")
+	}
+
+	return rest_errors.NewInternalServerError("error processing request", errors.New("parsing error"))
+}
---
+++ rest_errors/rest_errors.go
@@ -0,0 +1,50 @@
+package rest_errors
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type RestErr struct {
+	Message string        `json:"message"`
+	Status  int           `json:"status"`
+	Error   string        `json:"error"`
+	Causes  []interface{} `json:"causes"`
+}
+
+func EncodeRestErrToJson(restErr *RestErr) string {
+	restErrJson, err := json.Marshal(restErr)
+	if err != nil {
+		panic("error while marshalling RestErr")
+	}
+
+	return string(restErrJson)
+}
+
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusBadRequest,
+		Error:   "bad_request",
+	}
+}
+
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusNotFound,
+		Error:   "not_found",
+	}
+}
+
+func NewInternalServerError(message string, err error) *RestErr {
+	result := &RestErr{
+		Message: message,
+		Status:  http.StatusInternalServerError,
+		Error:   "internal_server_error",
+	}
+	if err != nil {
+		result.Causes = append(result.Causes, err.Error())
+	}
+	return result
+}
